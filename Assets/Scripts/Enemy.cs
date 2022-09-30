5538d5ed81e7881ed4ec9e29d09fe7b24bd70546
River1300 <94117169+River1300@users.noreply.github.com>
2022-09-30 23:30:33 +0900

    shootinggame_1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,339 +4,39 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // *목표 1 : 적을 구성하고 싶다.
-    // *풀이 :
-    //      1) 적을 구성하는 속성을 선언한다.
-    //      2) 플레이 중 스프라이트의 변화가 있으므로 Sprite[] 배열을 선언한다.
-    //      3) 플레이 중 스프라이트의 변화가 있으므로 SpriteRenderer 변수를 선언한다.
-    //      4) 적의 물리적인 움직임을 위해 Rigidbody2D 변수를 선언한다.
-    // *순서 :
-    //      1) 속성을 선언한다.
-    //      2) 속성을 초기화 한다.
-    //      3) 플레이어의 총알을 맞는 역활을 구성한다.
-    //      4) 총알을 맞으면 체력이 줄어 든다.
-    //      5) 체력이 모두 소모되면 제거된다.
-    //      6) 피격 될 때 스프라이트의 변화가 발생한다.
-    //      7) 스프라이트가 원상 복구 된다.
-    //      8) 적이 경계선에 닿으면 제거된다.
-    //      9) 총알에 맞으면 총알이 사라진다.
-    //      10) 프리팹으로 만든다.
-
-    // *추가 1 : 적이 옆에서도 나오기 때문에 무조건 아래로 내려가는 물리적 행위는 지운다.
-
-    public string enemyName;
-    public int enemyScore;
     public float speed;
     public int health;
-    public Sprite[] sprites;
 
-    public GameObject bulletFactoryA;
-    public GameObject bulletFactoryB;
-    public GameObject itemCoin;
-    public GameObject itemPower;
-    public GameObject itemBoom;
-    public GameObject player;
-    public ObjectManager objectManager;
-    public GameManager gameManager;
-
-    public float maxShotDelay;
-    public float currentDelay;
-
-    SpriteRenderer spriteRenderer;
-    Animator anim;
-
-    public int patternIndex;
-    public int curPatternCount;
-    public int[] maxPatternCount;
+    Rigidbody rigid;
 
     void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
-        if(enemyName == "B")
-        {
-            anim = GetComponent<Animator>();
-        }
+        rigid = GetComponent<Rigidbody>();
+        rigid.velocity = Vector3.down * speed;
     }
 
-    void OnEnable()
+    void OnHit(int dmg)
     {
-        switch(enemyName)
-        {
-            case "B" :
-                health = 3000;
-                Invoke("Stop", 2);
-                break;
-            case "L" :
-                health = 30;
-                break;
-            case "M" :
-                health = 10;
-                break;
-            case "S" :
-                health = 3;
-                break;
-        }
-    }
-
-    void Stop()
-    {
-        if(!gameObject.activeSelf) { return; }
-
-        Rigidbody2D rigid = GetComponent<Rigidbody2D>();
-        rigid.velocity = Vector2.zero;
-
-        Invoke("Think", 2);
-    }
-
-    void Think()
-    {
-        patternIndex = patternIndex == 3 ? 0 : patternIndex + 1;
-        curPatternCount = 0;
-
-        switch(patternIndex)
-        {
-            case 0:
-                FireForward();
-                break;
-
-            case 1:
-                FireShot();
-                break;
-
-            case 2:
-                FireArc();
-                break;
-
-            case 3:
-                FireAround();
-                break;
-        }
-    }
-
-    void FireForward()
-    {
-        GameObject bulletR = objectManager.MakeObj("BulletBossA");
-        bulletR.transform.position = transform.position + Vector3.right * 0.3f;
-        GameObject bulletRR = objectManager.MakeObj("BulletBossA");
-        bulletRR.transform.position = transform.position + Vector3.right * 0.6f;
-        GameObject bulletL = objectManager.MakeObj("BulletBossA");
-        bulletL.transform.position = transform.position + Vector3.left * 0.3f;
-        GameObject bulletLL = objectManager.MakeObj("BulletBossA");
-        bulletLL.transform.position = transform.position + Vector3.left * 0.6f;
-
-        Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
-        Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
-        Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
-        Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
-
-        rigidR.AddForce(Vector2.down * 3, ForceMode2D.Impulse);
-        rigidRR.AddForce(Vector2.down * 3, ForceMode2D.Impulse);
-        rigidL.AddForce(Vector2.down * 3, ForceMode2D.Impulse);
-        rigidLL.AddForce(Vector2.down * 3, ForceMode2D.Impulse);
-
-        curPatternCount++;
-
-        if(curPatternCount < maxPatternCount[patternIndex])
-        {
-            Invoke("FireForward", 2);
-        }
-        else
-        {
-            Invoke("Think", 3);
-        }
-    }
-
-    void FireShot()
-    {
-        for(int index = 0; index < 5; index++)
-        {
-            GameObject bullet = objectManager.MakeObj("BulletEnemyB");
-            bullet.transform.position = transform.position;
-            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
-
-            Vector2 dirVec = player.transform.position - transform.position;
-            Vector2 ranVec = new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(0f, 2f));
-            dirVec += ranVec;
-            rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
-        }
-
-        curPatternCount++;
-
-        if(curPatternCount < maxPatternCount[patternIndex])
-        {
-            Invoke("FireShot", 3.5f);
-        }
-        else
-        {
-            Invoke("Think", 3);
-        }
-    }
-
-    void FireArc()
-    {
-        GameObject bullet = objectManager.MakeObj("BulletEnemyA");
-        bullet.transform.position = transform.position;
-        bullet.transform.rotation = Quaternion.identity;
-
-        Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
-
-        Vector2 dirVec = new Vector2(Mathf.Cos
-        (Mathf.PI * 10 * curPatternCount / maxPatternCount[patternIndex]), -1);
-
-        rigid.AddForce(dirVec.normalized * 5, ForceMode2D.Impulse);
-
-        curPatternCount++;
-
-        if(curPatternCount < maxPatternCount[patternIndex])
-        {
-            Invoke("FireArc", 0.15f);
-        }
-        else
-        {
-            Invoke("Think", 3);
-        }
-    }
-
-    void FireAround()
-    {
-        int roundNumA = 50;
-        int roundNumB = 40;
-        int roundNum = curPatternCount % 2 == 0 ? roundNumA : roundNumB;
-        for(int index = 0; index < roundNum; index++)
-        {
-            GameObject bullet = objectManager.MakeObj("BulletBossB");
-            bullet.transform.position = transform.position;
-            bullet.transform.rotation = Quaternion.identity;
-
-            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
-
-            Vector2 dirVec = new Vector2(Mathf.Cos(Mathf.PI * 2 * index / roundNum), 
-                                        Mathf.Sin(Mathf.PI * 2 * index / roundNum));
-
-            rigid.AddForce(dirVec.normalized * 2, ForceMode2D.Impulse);
-
-            Vector3 rotVec = Vector3.forward * 360 * index / roundNum + Vector3.forward * 90;
-            bullet.transform.Rotate(rotVec);
-        }
-
-        curPatternCount++;
-
-        if(curPatternCount < maxPatternCount[patternIndex])
-        {
-            Invoke("FireAround", 0.7f);
-        }
-        else
-        {
-            Invoke("Think", 3);
-        }
-    }
-
-    void Update()
-    {
-        if(enemyName == "B") { return; }
-        Fire();
-        Reload();
-    }
-
-    void Fire()
-    {
-        if(currentDelay < maxShotDelay){ return; }
-
-        if(enemyName == "S")
-        {
-            GameObject bullet = objectManager.MakeObj("BulletEnemyA");
-            bullet.transform.position = transform.position;
-            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
-
-            Vector3 dirVec = player.transform.position - transform.position;
-            rigid.AddForce(dirVec.normalized * 7, ForceMode2D.Impulse);
-        }
-        else if(enemyName == "L")
-        {
-            GameObject bulletR = objectManager.MakeObj("BulletEnemyB");
-            bulletR.transform.position = transform.position + Vector3.right * 0.3f;
-            GameObject bulletL = objectManager.MakeObj("BulletEnemyB");
-            bulletL.transform.position = transform.position + Vector3.left * 0.3f;
-            Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
-            Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
-            Vector3 dirVecR = player.transform.position - (transform.position + Vector3.right * 0.3f);
-            Vector3 dirVecL = player.transform.position - (transform.position + Vector3.left * 0.3f);
-            rigidR.AddForce(dirVecR.normalized * 5, ForceMode2D.Impulse);
-            rigidL.AddForce(dirVecL.normalized * 5, ForceMode2D.Impulse);
-        }
-        currentDelay = 0;
-    }
-
-    void Reload()
-    {
-        currentDelay += Time.deltaTime;
-    }
-
-    public void OnHit(int dmg)
-    {
-        if(health <= 0) { return; }
         health -= dmg;
-
-        if(enemyName == "B")
-        {
-            anim.SetTrigger("OnHit");
-        }
-        else
-        {
-            spriteRenderer.sprite = sprites[1];
-            Invoke("ReturnSprite", 0.1f);
-        }
 
         if(health <= 0)
         {
-            Player playerLogic = player.GetComponent<Player>();
-            playerLogic.score += enemyScore;
-
-            int ran = enemyName == "B" ? 0 : Random.Range(0, 10);
-            if(ran < 5)
-            {
-                Debug.Log("Not Item");
-            }
-            else if(ran < 8)
-            {
-                GameObject itemCoin = objectManager.MakeObj("ItemCoin");
-                itemCoin.transform.position = transform.position;
-            }
-            else if(ran < 9)
-            {
-                GameObject itemPower = objectManager.MakeObj("ItemPower");
-                itemPower.transform.position = transform.position;
-            }
-            else if(ran < 10)
-            {
-                GameObject itemBoom = objectManager.MakeObj("ItemBoom");
-                itemBoom.transform.position = transform.position;
-            }
-
-            gameObject.SetActive(false);
-            transform.rotation = Quaternion.identity;
-            gameManager.CallExplosion(transform.position, enemyName);
+            Destroy(gameObject);
         }
     }
 
-    void ReturnSprite()
+    void OnTriggerEnter(Collider other)
     {
-        spriteRenderer.sprite = sprites[0];
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.gameObject.tag == "BorderBullet" && enemyName != "B")
+        if(other.gameObject.tag == "BoarderW")
         {
-            gameObject.SetActive(false);
-            transform.rotation = Quaternion.identity;
+            Destroy(gameObject);
         }
-        else if(other.gameObject.tag == "Player Bullet")
+        else if(other.gameObject.tag == "pBullet")
         {
-            Bullet bullet = other.gameObject.GetComponent<Bullet>();
-            OnHit(bullet.dmg);
-            other.gameObject.SetActive(false);
+            PlayerBullet bullet = other.gameObject.GetComponent<PlayerBullet>();
+            OnHit(bullet.damage);
+
+            Destroy(other.gameObject);
         }
     }
 }
